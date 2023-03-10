570883a76aecab25be11b776cdf5960852be930a
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-03-10 14:44:49 +0530

    bitra: Add libgui patch in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,4 +17,16 @@ else
 	git fetch https://github.com/Project-Elixir/frameworks_av/ c1da308642fc5bed9aa98bf07ca3d7a2beec717d && git cherry-pick FETCH_HEAD
 fi
 
+cd ..
+
+echo "Applying libgui patch"
+
+cd native || exit 1
+
+if git log -150 --oneline --decorate | grep "Add vendor version for libgui"; then
+	echo "Fix was already applied"
+else
+	git fetch https://github.com/LineageOS/android_frameworks_native/ c73885dcba344f2640d7faf1e375c03e7c5ffacc && git cherry-pick FETCH_HEAD
+fi
+
 cd ../..
